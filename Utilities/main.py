1e22668f5600824074a3a830e9afe212a6a99947
BrunoBarroso09 <brunomcbarroso@outlook.pt>
2026-02-03 19:58:46 +0000

    Refactor mask_email method and create valid_email method

--- Utilities/main.py
+++ Utilities/main.py
@@ -1,15 +1,26 @@
 import os
+import re
 
 class Utilities:
 
     @staticmethod
-    def mask_email(email: str) ->str:
+    def mask_email(email: str) -> str:
         #Privacy: Transform email 'example@email.com' to 'e******@email.com'
-        email_split = email.split('@')
-        print(f"{email_split[0][0]}*****@{email_split[1]}")
+        if not email or '@' not in email:
+            return 'Invalid email'
 
+        email_split = email.split('@')
+        user_email= email_split[0][0]
+        domain = email_split[1]
+
+        return f"{user_email}*****@{domain}"
+
+    @staticmethod
     def clear_terminal():
         #Clear terminal (works in Windows, Mac and Linux)
         os.system('cls' if os.name == 'nt' else 'clear')
 
-em = Utilities.mask_email('brunomcbarroso@gmail.com')
+    @staticmethod
+    def valid_email(email: str) -> bool:
+        regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        return bool(re.search(regex, email))

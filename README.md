5406af033344e8e699f2d1f6c77edc0f96b00861
BrunoBarroso09 <brunomcbarroso@outlook.pt>
2026-02-24 22:19:06 +0000

    Remove .idea from tracking. Create new method validate_iban to validade Portuguese IBAN

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This component give a lot of essential static methods to simplify repetitive tas
 ## ✨ Functionalities
 
 - **mask_email**: Sensitive data protection (GDPR friendly)
-- **clear_terminal**: Cross-platform utility for console cleaning.
 - **validate_email**: Strict formatting verification using Regex.
 - **format_currency**: Conversion of numerical values ​​to the European monetary format.
 - **get_env**: Securely read configurations via .env files.
 - **validate_fiscal_number**: Validates the Portuguese taxpayer identification number according to Portuguese standards.
 - **validate_postal_code**: Validates the format of the Portuguese postal code using regular expressions (Regex).
+- **validate_iban**: Validates a Portuguese IBAN (format and MOD 97 check)
 
 
 ## ⚙️ Dependencies
--- Utilities/utilities.py
+++ Utilities/utilities.py
@@ -1,6 +1,7 @@
 import os
 import re
 from dotenv import load_dotenv
+from typing import Optional
 
 #load_env outside the class because here load one time only
 load_dotenv()
@@ -11,70 +12,76 @@ class Utilities:
     def mask_email(email: str) -> str:
         if not email or '@' not in email:
             return 'Invalid email'
-        email_split = email.split('@')
-        user_email= email_split[0][0]
-        domain = email_split[1]
-        return f"{user_email}*****@{domain}"
-
-    @staticmethod #Clear terminal (works in Windows, Mac and Linux)
-    def clear_terminal() -> None:
-        os.system('cls' if os.name == 'nt' else 'clear')
+        user, domain = email.split('@',1)
+        domain, tld = domain.split('.',1)
+        masked_user = user[0] + '*' * len(user[1:])
+        masked_domain = domain[0] + '*' * len(domain[1:])
+        return f"{masked_user}@{masked_domain}.{tld}"
 
     @staticmethod #Validate if the current email is valid
     def validate_email(email: str) -> bool:
-        regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-        return bool(re.search(regex, email))
+        if not email or '@' not in email:
+            return False
+        regex = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+        return bool(re.fullmatch(regex, email))
 
     @staticmethod #Transform a number into a readable currency format.
     def format_currency(value: float, symbol: str = '€') -> str:
         return f"{value:,.2f} {symbol}".replace(",", "X").replace(".", ",").replace("X", ".")
 
-    @staticmethod #Read systems variables or .env file
-    def get_env(key: str, default: str = None) -> str:
+    @staticmethod #Read system variables .env file
+    def get_env(key: str, default: Optional[str] = None) -> Optional[str]:
         return os.getenv(key, default)
 
     @staticmethod
     def _validate_digit(digit: str) -> str:
-        try:
-            if not digit.isdigit():
-                raise ValueError("All characters need to to be digit")
-            if not len(digit) == 8:
-                raise ValueError("Number of digits different from 8 digits")
-
-            sum_digit = (
-                    int(digit[0]) * 9
-                    + int(digit[1]) * 8
-                    + int(digit[2]) * 7
-                    + int(digit[3]) * 6
-                    + int(digit[4]) * 5
-                    + int(digit[5]) * 4
-                    + int(digit[6]) * 3
-                    + int(digit[7]) * 2
-            )
-            rest = sum_digit % 11
-            if rest < 2:
-                return "0"
-            return str(11 - rest)
-        except ValueError:
-            raise ValueError("All characters need to to be digit")
+        if not digit.isdigit() or len(digit) != 8:
+            raise ValueError("All characters need to be digits and length must be 8")
+        sum_digit = (
+                int(digit[0]) * 9 + int(digit[1]) * 8 + int(digit[2]) * 7 +
+                int(digit[3]) * 6 + int(digit[4]) * 5 + int(digit[5]) * 4 +
+                int(digit[6]) * 3 + int(digit[7]) * 2
+        )
+        rest = sum_digit % 11
+        return "0" if rest < 2 else str(11 - rest)
 
     @staticmethod
     def validate_fiscal_number(fiscal_number: str) -> bool:
-        try:
-            if not int(fiscal_number.isdigit()) or len(fiscal_number) != 9:
-                return False
-            else:
-                return fiscal_number[-1] == Utilities._validate_digit(fiscal_number[:8])
-        except ValueError:
-            raise ValueError("Invalid fiscal number")
+        if not fiscal_number.isdigit() or len(fiscal_number) != 9:
+            return False
+        return fiscal_number[-1] == Utilities._validate_digit(fiscal_number[:8])
 
     @staticmethod
     def validate_postal_code(postal_code: str) -> bool:
-        try:
-            regex_cp = "^\\d{4}-\\d{3}?$"
-            if bool(re.match(regex_cp, postal_code)):
-                return True
-            else:
-                return False
-        except ValueError:
-            raise ValueError("Invalid postal code")
+        if not postal_code:
+            return False
+        regex_cp = r"\d{4}-\d{3}"
+        return bool(re.fullmatch(regex_cp, postal_code))
+
+    @staticmethod
+    def _calculate_mod97(iban : str) -> int:
+        code = iban[:4]
+        digits = iban[4:]
+        replace_code = code.replace("P", str(25)).replace("T", str(29))
+        iban_formatted = digits + replace_code
+        number = int(iban_formatted)
+        result = number % 97
+        return result
+
+    @staticmethod
+    def validate_iban(iban: str) -> bool:
+        if not iban:
+            return False
+        iban = iban.replace(" ", "")
+        if len(iban) != 25 or iban[:2] != "PT":
+            return False
+        if not iban[2:].isdigit():
+            return False
+
+        iban_valid = Utilities._calculate_mod97(iban)
+
+        return iban_valid == 1
+
+
+util = Utilities()
+util.validate_iban("PT50 0002 0123 1234 5678 9015 4")

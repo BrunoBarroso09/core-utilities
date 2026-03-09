2ee70e24c7e7a472a50102778f7357d81a4e434f
BrunoBarroso09 <brunomcbarroso@outlook.pt>
2026-03-09 22:49:03 +0000

    Refactor __init__ file

--- Utilities/__init__.py
+++ Utilities/__init__.py
@@ -1,5 +1,5 @@
 from .currency import format_currency
-from .env import get_env
+from .env import *
 from .validators import mask_email, validate_email, validate_fiscal_number, validate_iban, validate_postal_code, validate_telephone_number
 
 __all__ = [
